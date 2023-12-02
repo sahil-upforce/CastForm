5183646dd5d298fc8ea88b57acfcb808cc8438c3
Sahil Khirsaria <sahil@upforce.tech>
2023-12-02 20:04:11 +0530

    SETUP: setup pre-commit

--- settings/base.py
+++ settings/base.py
@@ -1,10 +1,13 @@
 import os
 
-from dotenv import load_dotenv, dotenv_values
+from dotenv import dotenv_values, load_dotenv
 
-
-env_maping_dict = {"development": "dev", "staging": "stage", "production": "prod"}
-env = os.getenv('ENVIRONMENT', 'development')
+env_maping_dict = {
+    "development": "dev",
+    "staging": "stage",
+    "production": "prod",
+}
+env = os.getenv("ENVIRONMENT", "development")
 dotenv_path = f".env.{env_maping_dict.get(env)}"
 
 if not load_dotenv(dotenv_path=dotenv_path):
@@ -15,4 +18,15 @@ if not load_dotenv(dotenv_path=dotenv_path):
 ENV_VARIABLES = dict(dotenv_values(dotenv_path=dotenv_path))
 
 django_project_name = ENV_VARIABLES.get("DJANGO_PROJECT_NAME", "CASTFORM")
-print(" "*50 + "\n" + "*"*50 + "\n" + f"Using {django_project_name}_ENV: " + (str(env).upper()) + "\n" + "*"*50 + "\n" + " "*50)
+print(
+    " " * 50
+    + "\n"
+    + "*" * 50
+    + "\n"
+    + f"Using {django_project_name}_ENV: "
+    + (str(env).upper())
+    + "\n"
+    + "*" * 50
+    + "\n"
+    + " " * 50
+)
--- settings/base_settings.py
+++ settings/base_settings.py
@@ -5,52 +5,52 @@ DEBUG = ENV_VARIABLES.get("DEBUG", False)
 ALLOWED_HOSTS = ENV_VARIABLES.get("DJANGO_ALLOWED_HOSTS", ["*"]).split(" ")
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-ROOT_URLCONF = 'CastForm.urls'
+ROOT_URLCONF = "CastForm.urls"
 
-WSGI_APPLICATION = 'CastForm.wsgi.application'
+WSGI_APPLICATION = "CastForm.wsgi.application"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': ENV_VARIABLES.get("DB_NAME"),
-        'USER': ENV_VARIABLES.get("DB_USER"),
-        'PASSWORD': ENV_VARIABLES.get("DB_PASSWORD"),
-        'HOST': ENV_VARIABLES.get("DB_HOST"),
-        'PORT': ENV_VARIABLES.get("DB_PORT"),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": ENV_VARIABLES.get("DB_NAME"),
+        "USER": ENV_VARIABLES.get("DB_USER"),
+        "PASSWORD": ENV_VARIABLES.get("DB_PASSWORD"),
+        "HOST": ENV_VARIABLES.get("DB_HOST"),
+        "PORT": ENV_VARIABLES.get("DB_PORT"),
     },
 }
 
@@ -61,14 +61,14 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
