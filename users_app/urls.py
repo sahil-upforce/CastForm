ba1768c67e2b5a75fdb4641b6759da93690cf996
Sahil Khirsaria <sahil@upforce.tech>
2023-12-20 10:10:17 +0530

    DEVELOPMENT: [IMPLEMENT] crud in user types

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,5 +1,6 @@
 from core.router import NoSlashRouter
-from users_app.views import GenderModelViewSet
+from users_app.views import GenderModelViewSet, UserTypeModelViewSet
 
 user_router = NoSlashRouter()
 user_router.register(r"genders", GenderModelViewSet)
+user_router.register(r"user-types", UserTypeModelViewSet)
--- users_app/views.py
+++ users_app/views.py
@@ -1,10 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.views import CustomDestroyMessageMixin
-from users_app.models import Gender
-from users_app.serializers import GenderSerializer
+from users_app.models import Gender, UserType
+from users_app.serializers import GenderSerializer, UserTypeSerializer
 
 
 class GenderModelViewSet(ModelViewSet, CustomDestroyMessageMixin):
     serializer_class = GenderSerializer
     queryset = Gender.objects.all()
+
+
+class UserTypeModelViewSet(ModelViewSet, CustomDestroyMessageMixin):
+    serializer_class = UserTypeSerializer
+    queryset = UserType.objects.all()
