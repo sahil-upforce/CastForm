061daf4abf363efb6eb2d73995d1b5513d6ea523
Sahil Khirsaria <sahil@upforce.tech>
2023-12-28 23:14:17 +0530

    DEVELOPMENT: [IMPLEMENT] create user registartion view

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,6 +1,7 @@
 from core.router import NoSlashRouter
-from users_app.views import GenderModelViewSet, UserTypeModelViewSet
+from users_app import views
 
 user_router = NoSlashRouter()
-user_router.register(r"genders", GenderModelViewSet)
-user_router.register(r"user-types", UserTypeModelViewSet)
+user_router.register(r"genders", views.GenderModelViewSet)
+user_router.register(r"user-types", views.UserTypeModelViewSet)
+user_router.register(r"account", views.UserModelViewSet)
